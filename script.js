e0cb2a45b852d5274a0ceddaa958e57bbcb4b7ea
lukiakos <lukiakos@gmail.com>
2022-06-25 13:06:09 +0200

    new functionality
    Added clear board, generate new board functionality as well as a colour picker.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const grid = document.getElementById('etcha');
-console.log(grid)
 
-let size = 16;
+let size = 10;
+let brushColor = 'black';
 
 function createGrid (size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -9,10 +9,59 @@ function createGrid (size) {
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement("div");
         gridElement.classList.add('grid-element');
-        gridElement.addEventListener('mouseover', console.log('nice'));
-        gridElement.addEventListener('mousedown', console.log('nice'));
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('click', changeColor)
+        gridElement.addEventListener('dragstart', (e) => {
+            e.preventDefault()
+        })
+        gridElement.addEventListener('drop', (e) => {
+            e.preventDefault()
+        })
         grid.appendChild(gridElement);
     }
+}
+
+function changeColor(e) {
+    if (e.buttons >= 1 || e.type == "click") {
+        if (e.altKey == true) {
+            e.target.style.backgroundColor = 'white';
+        } else {
+            e.target.style.backgroundColor = `${brushColor}`
+        }
+    }
+}
+
+const clearBtn = document.querySelector('.clear')
+clearBtn.addEventListener('click', clearGrid)
+const colorPicker = document.querySelector('.colorPicker')
+colorPicker.addEventListener("change", watchColorPicker, false);
+const gridSizeSlider = document.querySelector('.gridSizeSlider');
+gridSizeSlider.addEventListener("change", watchGridSizeSlider, false);
+const newGridBtn = document.querySelector("#createNewGrid");
+newGridBtn.addEventListener("click", createNewGrid);
+const sizeMeter = document.querySelector("#gridSizeMeter");
+sizeMeter.textContent = size + "x" + size;
+
+function watchColorPicker(e) {
+    brushColor = e.target.value;
+}
+
+function watchGridSizeSlider(e) {
+    size = e.target.value;
+    sizeMeter.textContent = size + "x" + size;
+}
+
+
+
+function clearGrid() {
+    let gridItems = document.querySelectorAll('.grid-element');
+    gridItems.forEach(e => e.style.backgroundColor = 'white');
+}
+
+function createNewGrid() {
+    let gridItems = document.querySelectorAll('.grid-element');
+    gridItems.forEach(e => e.remove());
+    createGrid(size);
 }
 
 window.onload = () => {

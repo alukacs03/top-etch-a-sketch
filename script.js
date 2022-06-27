a654dd346cf0339dca50601bcd6f0a5fb9705bfa
lukiakos <lukiakos@gmail.com>
2022-06-27 11:17:36 +0200

    Cleaned up the code and commented
    Cleaned up the javascript and css, as well as added comments to most functionalities.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const grid = document.getElementById('etcha');
 
+// creating and setting default values to variables
+
 let size = 10;
 let brushColor = 'black';
 let rainbowMode = false;
-let grayScaleMode = false;
 let normalMode = true;
 let isGridOn = true;
 
 
+// function used to create the grid
 function createGrid (size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -16,7 +18,7 @@ function createGrid (size) {
         gridElement.classList.add('grid-element');
         gridElement.addEventListener('mouseover', changeColor)
         gridElement.addEventListener('click', changeColor)
-        gridElement.addEventListener('dragstart', (e) => {
+        gridElement.addEventListener('dragstart', (e) => { // used so that when dragging the mouse over the grid, the annoying dragging icon doesn't show up
             e.preventDefault()
         })
         gridElement.addEventListener('drop', (e) => {
@@ -28,7 +30,7 @@ function createGrid (size) {
 
 function changeColor(e) {
     if (e.buttons >= 1 || e.type == "click") {
-        if (e.ctrlKey == true) {
+        if (e.ctrlKey == true) { // while ctrl is pressed, eraser is activated
             e.target.style.backgroundColor = 'white';
         } else if (normalMode) {
             e.target.style.backgroundColor = `${brushColor}`
@@ -41,6 +43,7 @@ function changeColor(e) {
     }
 }
 
+// button declarations, adding eventlisteners and triggering the respective functions
 const clearBtn = document.querySelector('.clear')
 clearBtn.addEventListener('click', clearGrid)
 const colorPicker = document.querySelector('.colorPicker')
@@ -49,6 +52,7 @@ const gridSizeSlider = document.querySelector('.gridSizeSlider');
 const newGridBtn = document.querySelector("#createNewGrid");
 newGridBtn.addEventListener("click", createNewGrid);
 const sizeMeter = document.querySelector("#gridSizeMeter");
+sizeMeter.textContent = size + "x" + size;
 const rainbowBtn = document.querySelector(".rainbowBtn");
 rainbowBtn.addEventListener("click", changeMode);
 const normalBtn = document.querySelector(".normalBtn");
@@ -64,54 +68,60 @@ etchaIcon.addEventListener("click", closeWindow);
 
 function closeWindow() {
     let progWindow = document.querySelector(".etchadiv");
-    progWindow.classList.toggle('hidden')
+    if (progWindow.style.display === "none") {
+        progWindow.style.display = "flex";
+    } else {
+        progWindow.style.display = "none";
+    }
 }
 
+// toggles the class "unbordered" on all the grid items, which is set up in CSS to set border to 0
 function toggleGrid() {
     let gridItems = document.querySelectorAll('.grid-element');
     gridItems.forEach(e => e.classList.toggle('unbordered'));
 }
 
+// triggered  by thte Rainbow and Normal mode buttons
 function changeMode() {
     if (this.className === "rainbowBtn") {
         normalMode = false;
-        grayScaleMode = false;
         rainbowMode = true;
     } else if (this.className === "normalBtn") {
         rainbowMode = false;
-        grayScaleMode = false;
         normalMode = true;
     }
 }
 
-function downloadImage () {
+// image downloader
+function downloadImage () { 
     html2canvas(document.querySelector(".etcha"), {
-        onrendered: function(canvas)
-        {
-        canvas.toBlob(function(blob) {
-        saveAs(blob, "etchasketch95_image.png");
-        });
+        onrendered: function(canvas) {
+            canvas.toBlob(function(blob) {
+            saveAs(blob, "etchasketch95_image.png");
+            });
         }
-        });
+    });
 }
 
-
+// set size based on slider, as well as display it under it
 gridSizeSlider.oninput = function () {
     size = this.value;
     sizeMeter.textContent = size + "x" + size;
 }
-sizeMeter.textContent = size + "x" + size;
 
+// set brush color based on color picker
 function watchColorPicker(e) {
     brushColor = e.target.value;
 }
 
-
+// clear the grid by setting all the grid-elemnt backgrounds to white
 function clearGrid() {
     let gridItems = document.querySelectorAll('.grid-element');
     gridItems.forEach(e => e.style.backgroundColor = 'white');
 }
 
+// create a new grid based on the size set with the slider
+// removes the previous grid then triggers the createGrid function
 function createNewGrid() {
     let gridItems = document.querySelectorAll('.grid-element');
     gridItems.forEach(e => e.remove());
